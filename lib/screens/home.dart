73808eb46f59840f44f3b824d8b4a7a40ba26b1a
trinhxuanhoa <bongnaiton30@gmail.com>
2025-03-20 19:01:10 +0700

    d2

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'translate.dart';
+import 'dart:math';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,164 +7,222 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.all(10), // Tạo khoảng cách với mép màn hình
-        decoration: BoxDecoration(
-          color: Colors.white, // Màu nền bên trong viền
-          border: Border.all(
-            color: Colors.black, // Màu viền
-            width: 2, // Độ dày viền
-          ),
-          borderRadius: BorderRadius.circular(20), // Bo góc viền
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              space(50),
-              DICTIONARY(), // tiêu đề kích thước 4% so với chiều dài
-              space(50),
-              search(screenWidth * 0.5 + 40), // thanh tìm kiếm
-              space(50),
-              buildIconGrid(context, screenWidth * 0.25, screenHeight * 0.25),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  // tạo khoảng cách
-  Widget space(double height) {
-    return SizedBox(height: height);
-  }
-
-  // tạo tiêu đề
-  Widget DICTIONARY() {
-    return Center(
-      child: Text(
-        "DICTIONARY",
-        style: TextStyle(
-          fontSize: 40, // Cỡ chữ = 4% chiều rộng màn hình
-          fontWeight: FontWeight.bold,
-          color: Colors.black,
-        ),
-        overflow: TextOverflow.clip, // Giới hạn hiển thị trong một dòng
-        softWrap: false, // Ngăn không cho chữ xuống dòng
-      ),
-    );
-  }
-
-  // tạo thanh tìm kiếm
-  Widget search(double width) {
-    return  SizedBox(
-      width: width, // Chiều rộng 50% màn hình
-
-      child: Container(
-        color: Colors.white, // Đặt màu nền xám
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: "Nhập từ bạn muốn tìm kiếm",
-            hintStyle: TextStyle(
-                fontSize: 15,
-                //height: screenHeight * 0.08, // Chiều cao 10% màn hình
-                color: Colors.grey[600]), // Màu chữ gợi ý
-            filled: true,
-            fillColor: Colors.grey[300], // Màu nền xám nhạt
-            prefixIcon: IconButton(
-              icon: Icon(Icons.search, color: Colors.grey[600]),
-              onPressed: () {
-                print("Tìm kiếm..."); // xử lý sự kiện
-              },
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade300,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.blue.shade100,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(Icons.person, color: Colors.blue.shade700, size: 20),
             ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30), // Bo tròn
-              borderSide: BorderSide.none, // Ẩn viền
-            ),
-            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 20), // Khoảng cách nội dung
+            onPressed: () {},
           ),
-        ),
-      ),
-    );
-  }
-
-  // Hàm tạo lưới 4 ô vuông
-  Widget buildIconGrid(BuildContext context, double width, double height) {
-    double minHeight = 90; // Giới hạn chiều cao tối thiểu
-    double maxHeight = 200; // Giới hạn chiều cao tối đa
-    double minHeightBox = minHeight*2+110;
-    double maxHeightBox = maxHeight*2+110;
-    return SizedBox(
-      width: width*2+40,
-      height: (height*2+110).clamp(minHeightBox,maxHeightBox),
-    child:   Padding(
-      padding: EdgeInsets.zero,
-      child: GridView.count(
-        crossAxisCount: 2, // 2 cột
-        crossAxisSpacing: 40, // Khoảng cách ngang
-        mainAxisSpacing: 30, // Khoảng cách dọc
-        childAspectRatio: width/height.clamp(minHeight, maxHeight), // Tỷ lệ chiều rộng / chiều cao = 1 (hình vuông)
-        children: [
-
-          buildSquareItem(Icons.translate, "Dịch văn bản", context,() {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Translate()),
-            );},width, height),
-
-          buildSquareItem(Icons.note_add , "Thêm từ", context,() {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Translate()),
-            );},width, height),
-
-          buildSquareItem(Icons.style , "Flashcard", context,() {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Translate()),
-            );},width, height),
-
-          buildSquareItem(Icons.games, "Minigame", context,() {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Translate()),
-            );},width, height),
-
         ],
       ),
-      ),
-    );
-  }
-
-  // Hàm tạo ô vuông có icon + chữ
-  Widget buildSquareItem(IconData icon, String label, BuildContext context,
-      VoidCallback onTap, double width, double height) {
-    return GestureDetector(
-      onTap: onTap, // Gọi hàm khi nhấn vào
-      child: SizedBox(
-        width: width,
-        height: height,
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[300], // Màu nền xám
-            borderRadius: BorderRadius.circular(10), // Bo góc nhẹ
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, // Căn giữa dọc
-            children: [
-              Icon(icon, size: (width * 0.1).clamp(8, 200), color: Colors.black), // Biểu tượng
-              SizedBox(height: 5), // Khoảng cách giữa icon và chữ
-              Text(
-                label,
-                style: TextStyle(fontSize: (width * 0.1).clamp(8, 200)),
-              ),
-            ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue.shade50, Colors.white],
+            stops: const [0.3, 1.0],
           ),
         ),
-      ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+
+              child: Column(
+                children: [
+                  const SizedBox(height: 10),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue.shade100,
+                          blurRadius: 10,
+                          spreadRadius: 5,
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.book,
+                      size: 50,
+                      color: Colors.blue.shade700,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text(
+                    softWrap: false, // Ngăn không cho chữ xuống dòng
+                    'DICTIONARY',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue.shade800,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    width: screenWidth / 2,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blue.shade100,
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                        ),
+                      ],
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        prefixIcon: IconButton(
+                          icon: Icon(Icons.search, color: Colors.blue.shade700),
+                          onPressed: () {},
+                        ),
+                        hintText: 'Nhập từ cần tìm kiếm',
+                        hintStyle: TextStyle(color: Colors.blue.shade300),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  buildIconGrid(context, screenWidth/2+16)
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
+// tạo lưới ô
+Widget buildIconGrid(BuildContext context, double width) {
+
+  double space = 16;
+
+  return Expanded(
+    child: LayoutBuilder(
+      builder: (context, constraints) {
+        double availableHeight = constraints.maxHeight; // Lấy chiều cao còn lại
+        return Center(
+          child: SizedBox(
+            width: width,
+            height: availableHeight,
+            child: GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: space,
+              mainAxisSpacing: space,
+              childAspectRatio: (width-space) / (availableHeight-space).clamp(145, 9999), // Căn chỉnh tỷ lệ kích thước ô
+              children: [
+                FeatureButton(
+                  icon: Icons.translate,
+                  label: 'Dịch văn bản',
+                  color: Colors.blue.shade600,
+                  height: (availableHeight-space)/2,
+                  onTap: () {
+
+                  },
+                ),
+                FeatureButton(
+                  icon: Icons.add_circle_outline,
+                  label: 'Thêm từ',
+                  color: Colors.purple.shade400,
+                  height: (availableHeight-space)/2,
+                  onTap: () {
+
+                  },
+                ),
+                FeatureButton(
+                  icon: Icons.card_membership,
+                  label: 'Flashcard',
+                  color: Colors.teal.shade500,
+                  height: (availableHeight-space)/2,
+                  onTap: () {
+
+                  },
+                ),
+                FeatureButton(
+                  icon: Icons.games,
+                  label: 'Minigame',
+                  color: Colors.amber.shade700,
+                  height: (availableHeight-space)/2,
+                  onTap: () {
+
+                  },
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
+// lớp ô vuông
+class FeatureButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final Color color;
+  final double height;
+  final VoidCallback onTap;
+
+  const FeatureButton({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.color,
+    required this.height,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double space = 16;
+    double width = (MediaQuery.of(context).size.width/2-space)/2;
+
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Container(
+        width: 100,
+        height: 50,
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, size: (min(width,height)*0.19).clamp(15, 200), color: color),
+            const SizedBox(height: 5),
+            Text(
+              label,
+              textAlign: TextAlign.center,
+              softWrap: false, // Ngăn không cho chữ xuống dòng
+              style: TextStyle(
+                fontSize: (min(width,height)*0.19).clamp(10, 200),
+                fontWeight: FontWeight.bold,
+                color: color,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}*/
