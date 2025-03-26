278e6fd7946936afc55d38a410b90793aa9bec25
quoctuan2005 <nqtuan05@gmail.com>
2025-03-26 07:27:18 +0700

    register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart'; // Import màn hình Home từ thư mục screens
+import 'authentic/register_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(), // Chạy màn hình chính
+      home: RegisterScreen(), // Chạy màn hình chính
     );
   }
 }
